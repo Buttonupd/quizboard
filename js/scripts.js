2ab8b71f70e373ef19df710082fe72afcad5374a
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-22 11:50:46 +0300

    declare variable 'name' in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var total = 0;
+var names= function(){
+  var name = parseInt(["year", "htmlType", "htmlMeaning", "creator", "htmlSec", "stdx", "ecma", "dataType"].value());
+  return(+=name);
+}
+//
 
 $(document).ready(function(){
   $("#proceed").click(function(){
@@ -5,4 +11,15 @@ $(document).ready(function(){
     $("#form1").fadeOut();
     $("#form2").fadeIn();
   });
+
+$("#submit").click(function(check){
+  event.preventDefault();
+  $("#form2").fadeOut();
+  $("#form3").show();
+
+  var name = result
+
+  $("#message").text(result);
+});
+
 });
