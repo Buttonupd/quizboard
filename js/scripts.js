13ee743b296ba53003117f08d0fc508f596b5999
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-24 18:55:38 +0300

    ctreate array with radio button names

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
 var total = 0;
-function myFunc() {
-  var q1 = document.getElementsByName('year');
-    if(q1[0].checked){
-      total===12.5;
-    }else{
-      total===0;
-    };
-  var q2 = document.getElementsByName('htmlType');
-    if(q2[0].checked){
-      total===12.5;
-    }else{
-      total===0;
-    };
-    var q3 = document.getElementsByName('htmlMeaning');
-      if(q3[1].checked){
-        total===12.5;
-      }else{
-        total===0;
-      };
-      total = q1+q2+q3;
+function myFunc(userValue) {
+  var names = ["year", "htmlType", "htmlMeaning", "creator", "htmlSec", "stdx", "ecma", "dataType"]
+  // var q1 = document.getElementsByName('year');
+  //   if(q1[0].checked){
+  //     total===12.5;
+  //   }else{
+  //     total===0;
+  //   };
+  // var q2 = document.getElementsByName('htmlType');
+  //   if(q2[0].checked){
+  //     total===12.5;
+  //   }else{
+  //     total===0;
+  //   };
+  //   var q3 = document.getElementsByName('htmlMeaning');
+  //     if(q3[1].checked){
+  //       total===12.5;
+  //     }else{
+  //       total===0;
+  //     };
+  //     total = q1+q2+q3;
 
 };
 
