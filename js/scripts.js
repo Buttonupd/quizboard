b00ce01064d75fcdf166add529f3706166208df3
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-24 18:53:14 +0300

    add id's to the individual radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,26 @@ var total = 0;
 function myFunc() {
   var q1 = document.getElementsByName('year');
     if(q1[0].checked){
-      total=12.5;
+      total===12.5;
     }else{
-      total=0;
+      total===0;
     };
   var q2 = document.getElementsByName('htmlType');
     if(q2[0].checked){
-      total=12.5;
+      total===12.5;
     }else{
-      total=0;
+      total===0;
     };
     var q3 = document.getElementsByName('htmlMeaning');
       if(q3[1].checked){
-        total=12.5;
+        total===12.5;
       }else{
-        total=0;
+        total===0;
       };
-  total = q1+q2+q3;
+      total = q1+q2+q3;
+
 };
+
 $(document).ready(function(){
   $("#proceed").click(function(){
     event.preventDefault();
@@ -32,9 +34,9 @@ $("#submit").click(function(){
   $("#form2").fadeOut();
   $("#form3").show();
 
+  alert(total.length);
 
-
-  $("#message").text(total);
+  // $("#message").text(total);
 
 });
 });
