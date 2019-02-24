961ca69766a18fcf1994412f9d717b6d98ebebca
System Administrator <root@Owners-MacBook-Pro.local>
2019-02-24 21:18:14 +0300

    add fireworks plugin to javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var total = 0;
 function myFunc(userValue) {
-  var names = ["year", "htmlType", "htmlMeaning", "creator", "htmlSec", "stdx", "ecma", "dataType"];
-  names.forEach(function(name) {
+    var names = ["year", "htmlType", "htmlMeaning", "creator", "htmlSec", "stdx", "ecma", "dataType"];
+    names.forEach(function(name) {
     var userValue = $("input:radio[name=" + name + "]:checked").val();
     if(userValue === "12.5") {
       total+=12.5;
@@ -25,7 +25,7 @@ $("#submit").click(function(){
   if(total >= 80) {
      $("#comments").text("EXCELLENT PERFOMANCE!!!");
      $("#comments").addClass('animated heartBeat');
-
+     $("#form3").fireworks();
    } else if(total >= 50) {
      $("#comments").text("Good work!");
      $("#comments").addClass('animated bounce');
